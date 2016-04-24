c4a8e044ac4932fe814a0514845643ce15ca0855
vvv <test@test.ru>
2016-04-24 22:10:40 +0300

    add @FindBy

--- SeleniumTrainSecond/src/test/java/pageObjects/Login.java
+++ SeleniumTrainSecond/src/test/java/pageObjects/Login.java
@@ -2,10 +2,13 @@ package pageObjects;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Login {
     By loginInput = By.id("username");
     By passInput = By.id("password");
-    By loginButton = By.cssSelector("button.radius");
+//4th review    By loginButton = By.cssSelector("button.radius");
     By greenMessage = By.id("flash");
 
     public WebDriver _driver;
@@ -23,9 +26,17 @@ public class Login {
         _driver.findElement(passInput).sendKeys(password);
     }
 
-    public void clickLogin(){
+/*4th review    public void clickLogin(){
         _driver.findElement(loginButton).click();
+    }*/
+
+    @FindBy(css="button.radius")
+    WebElement loginbutton;
+
+    public void clickLogin(){
+        loginbutton.click();
     }
+
 
     public boolean isLogged(){
         return _driver.findElement(greenMessage).isDisplayed();
