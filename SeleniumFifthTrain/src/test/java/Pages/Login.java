1fd9ce88bd1b33c04ff6e4ac1c82cafa502a81a8
vvv <test@test.ru>
2016-04-28 09:41:27 +0300

    start train

---
+++ SeleniumFifthTrain/src/test/java/Pages/Login.java
@@ -0,0 +1,7 @@
+package Pages;
+
+/**
+ * Created by Admin on 28.04.16.
+ */
+public class Login {
+}
---
+++ SeleniumFifthTrain/src/test/java/Tests/LoginTest.java
@@ -0,0 +1,7 @@
+package Tests;
+
+/**
+ * Created by Admin on 28.04.16.
+ */
+public class LoginTest {
+}
